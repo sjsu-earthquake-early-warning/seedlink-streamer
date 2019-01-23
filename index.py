740477e392ff1d3e9366b58d7bb0edee5871169f
Patrick Garcia <pkmgarcia@gmail.com>
2019-01-23 15:09:32 -0800

    Initial AWS Lambda integration.

--- index.py
+++ index.py
@@ -1,28 +1,33 @@
 #!usr/bin/python
-""" TODO: Invoke Lambda function """
-
 import waveformfetcher
-# import boto3
-# client = boto3.client('lambda')
+import boto3
+import json
+
+def send_to_lambda(client, data):
+  # Format data
+  payload = {
+    "data": data
+  }
+  payload = json.dumps(payload).encode('utf-8')
+
+  response = client.invoke(
+    FunctionName='hello-world',
+    # InvocationType='RequestResponse',    # Invoke synchronously (just for dev purposes)
+    InvocationType='Event',              # Invoke asynchronously (we don't need to wait)
+    Payload=payload
+  )
+  # print(response['Payload'].read())
 
 def main():
-  # Initialize waveform.
+  # Initalize boto3, fetcher
+  client = boto3.client('lambda')
   fetcher = waveformfetcher.WaveformFetcher()
-  
-  # To simulate continuous fetching,
-  for x in range(5):
-    st = fetcher.fetch_past(10000)
-    st.plot()
 
-  # Below is how to invoke a Lambda function.
-  # response = client.invoke(
-    # FunctionName='string',
-    # InvocationType='Event',
-    # LogType='None'|'Tail',
-    # ClientContext='string',
-    # Payload=b'bytes'|file,
-    # Qualifier='string'
-  # )
+  # To simulate continuous fetching,
+  for x in range(1):
+    st = fetcher.fetch_past(3000)
+    data = list(map(lambda x: int(x), st[0].data.tolist()))
+    send_to_lambda(client, data)
 
   return
 
